8dd3495c1697e30ba8283183ab4603696e378711
Tyler Florez <qualiabyte@gmail.com>
2014-07-27 02:50:47 -0700

    Change widget id to class

--- src/bitcoin.jsx
+++ src/bitcoin.jsx
@@ -1,5 +1,5 @@
 /** @jsx React.DOM */
 React.renderComponent(
   <div class="bitcoin-price">BTC/USD: $1.00</div>,
-  document.getElementById('bitcoin')
+  document.getElementsByClassName('bitcoin-widget')[0]
 );
