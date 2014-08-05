deeac9f553b85ce2fcd4bfa940f2e8fcab42fb57
Tyler Florez <qualiabyte@gmail.com>
2014-08-05 06:44:43 -0700

    Add bitcoin price component class

--- src/bitcoin.jsx
+++ src/bitcoin.jsx
@@ -1,5 +1,42 @@
 /** @jsx React.DOM */
+
+var BitcoinPrice = React.createClass({
+
+  getInitialState: function() {
+    return {
+      bitcoinPrice: null
+    };
+  },
+
+  componentDidMount: function() {
+    var self = this;
+    var url = 'https://blockchain.info/ticker?cors=true';
+    var success = function(data) {
+      if (self.isMounted()) {
+        self.setState({
+          bitcoinPrice: data['USD']['15m']
+        });
+      }
+    };
+    $.ajax(url).done(success);
+  },
+
+  render: function() {
+    return (
+      <div>
+        <span class="bitcoin-price-value">${this.state.bitcoinPrice}</span>
+        <span class="bitcoin-price-label"> BTC/USD</span>
+      </div>
+    );
+  }
+});
+
 React.renderComponent(
-  <div class="bitcoin-price">BTC/USD: $1.00</div>,
-  document.getElementsByClassName('bitcoin-widget')[0]
+  <BitcoinPrice />,
+  $('.bitcoin-price')[0]
+);
+
+React.renderComponent(
+  <BitcoinBalance />,
+  $('.bitcoin-balance')[0]
 );
