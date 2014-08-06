039a8ef9d0c15a4776fa41cb4619e53a94c3c855
Tyler Florez <qualiabyte@gmail.com>
2014-08-06 10:37:16 -0700

    Add bitcoin balance component and styles

--- src/bitcoin.jsx
+++ src/bitcoin.jsx
@@ -24,19 +24,62 @@ var BitcoinPrice = React.createClass({
   render: function() {
     return (
       <div>
-        <span class="bitcoin-price-value">${this.state.bitcoinPrice}</span>
-        <span class="bitcoin-price-label"> BTC/USD</span>
+        <span className="widget-label">Bitcoin Price</span>
+        <span className="price-value">${this.state.bitcoinPrice}</span>
+        <span className="price-label"> BTC/USD</span>
       </div>
     );
   }
 });
 
-React.renderComponent(
-  <BitcoinPrice />,
-  $('.bitcoin-price')[0]
-);
+var BitcoinBalance = React.createClass({
 
-React.renderComponent(
-  <BitcoinBalance />,
-  $('.bitcoin-balance')[0]
-);
+  getInitialState: function() {
+    return {
+      bitcoinBalance: null
+    };
+  },
+
+  componentDidMount: function() {
+    var self = this;
+    var url = 'https://blockchain.info/q/addressbalance/' + self.props.address + '?cors=true';
+    var success = function(data) {
+      var balance = data / 1E8;
+      if (self.isMounted()) {
+        self.setState({
+          bitcoinBalance: balance.toFixed(2)
+        });
+      }
+    };
+    $.ajax(url).done(success);
+  },
+
+  render: function() {
+    return (
+      <div>
+        <span className="widget-label">Bitcoin Balance</span>
+        <span className="balance">
+          <span className="balance-value">{this.state.bitcoinBalance}</span>
+          <span className="balance-units"> BTC</span>
+        </span>
+        <span className="bitcoin-address">{this.props.address}</span>
+      </div>
+    );
+  }
+});
+
+$('.coinlens.bitcoin-price').each(function(index, elem) {
+  var $price = $(elem);
+  React.renderComponent(
+    <BitcoinPrice />,
+    $price[0]
+  );
+});
+
+$('.coinlens.bitcoin-balance').each(function(index, elem) {
+  var $balance = $(elem);
+  React.renderComponent(
+    <BitcoinBalance address={$balance.data('address')}/>,
+    $balance[0]
+  );
+});
